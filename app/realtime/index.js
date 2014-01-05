f2d229979261bbdc4871aa375356e7d58a27fc5d
josedeweb <josealvarez712@gmail.com>
2014-01-05 13:13:52 -0500

    @user as name

--- app/realtime/index.js
+++ app/realtime/index.js
@@ -4,10 +4,10 @@ var Twitter = new Twit(auth);
 var stream 	= Twitter.stream('statuses/filter', { track: ['#soytanhipster'] });
 
 module.exports = function ( io ){
-	stream.on('tweet', function ( tweet ) {		
+	stream.on('tweet', function ( tweet ) {				
     	io.sockets.emit('tweet', 
     		{    			
-    			name  : tweet.user.name,
+    			name  : tweet.user.screen_name,
     			image : tweet.user.profile_image_url,
     			text  : tweet.text
     		}
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,7 +2,7 @@ var main = function () {
 	window.client = io.connect(window.location.href);
 	client.on('tweet', function (data) {		
 		$("section figure img").attr({'src':data.image});
-		$("section figure figcaption").html(data.name);
+		$("section figure figcaption").html("@"+data.name);
 		$("section article").html(data.text);	
 	});
 }
